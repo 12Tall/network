d5239f6a854795370baa51b21748944c363fe517
12tall <fb.ouyang@outlook.com>
2020-04-30 08:56:31 +0800

    AD 域查询

--- ldap/index.js
+++ ldap/index.js
@@ -6,5 +6,21 @@ var client = ldap.createClient({
 // 组是group?
 client.bind('cn=u01,ou=hr,dc=test,dc=com', 'Test123', function (err) {
     // assert.ifError(err);
+
+    client.search('dc=test,dc=com', {scope:'sub',filter:'(cn=u01)' }, (err, res) => {
+        console.log(err)
+        res.on('searchEntry', function (entry) {
+            console.log('entry: ' + JSON.stringify(entry.object));
+        });
+        res.on('searchReference', function (referral) {
+            console.log('referral: ' + referral.uris.join());
+        });
+        res.on('error', function (err) {
+            console.error('error: ' + err.message);
+        });
+        res.on('end', function (result) {
+            console.log('status: ' + result.status);
+        });
+    });
     console.log(err)
 });
