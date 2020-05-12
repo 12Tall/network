7ecad6ff6bb85c391915c440eb2d67d3018228db
12tall <fb.ouyang@outlook.com>
2020-05-12 16:08:01 +0800

    接口自动注册：   1. 装饰器可以使用异步方法；

--- srv/src/controller/user.js
+++ srv/src/controller/user.js
@@ -1,4 +1,4 @@
-const { service,dbpool } = require('@src/helpers');
+const { service, dbpool } = require('@src/helpers');
 
 // 这里增加一个Controller 装饰器，应该能保证接口的唯一性
 class User {
@@ -7,12 +7,17 @@ class User {
 
     // 这里也可以不传入ctx，但是传入的话会更灵活
     // 传入上下文的话，会不会造成项目耦合严重
-    @service('用户', '123')
+    @service()
     static async add(ctx, next) {
         let [res, field] = await dbpool.mysql.execute(`SELECT 1 FROM user WHERE id =2 LIMIT 1`);
         console.log(res[0]);
         console.log('添加用户');
         ctx.body = 'user';
+    }
+
+    @service()
+    static async update(ctx, next) {
+        console.log('123')
     }
 }
 
