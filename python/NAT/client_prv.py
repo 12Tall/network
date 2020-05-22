01183bc83771444cbe992d48248ac6987179ac8f
12tall <fb.ouyang@outlook.com>
2020-05-22 17:02:50 +0800

    虚拟机测试成功 下一步内网穿透

--- python/NAT/client_prv.py
+++ python/NAT/client_prv.py
@@ -1,88 +1,61 @@
 import socket
 import select
-
-from time import sleep
-
-
-binf_info = ('0.0.0.0', 9999)
-# cli for public API
-cli = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cli.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-# main srv
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-cli.bind(binf_info)
-server.bind(binf_info)
-server.listen(5)
-print('app running...')
+import threading
 
 
-inputs = [server, cli]
-outputs = []
-
-# single connection
-cli_in = None
-cli_out = None
+src_port = ('0.0.0.0', 9999)
+target_port = ('localhost', 3389)
 BUFFER_SIZE = 8*1024
-while True:
-    r, w, e = select.select(inputs, outputs, inputs)
-    print('recv sth. ...')
 
-    for s in r:
-        if s is server:
-            # create new conn for incoming
-            if cli_in is not None:
-                inputs.remove(cli_in)
-                cli_in.close()
-            print('new connection')
-            cli_in, addr_in = s.accept()
-            cli_in.setblocking(False)
-            inputs.append(cli_in)
 
-            # create a new cli for localhost:3389
-            if cli_out is not None:
-                inputs.remove(cli_out)
-                cli_out.close()
-            cli_out = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            cli_out.connect(('localhost', 3389))
-            cli_out.setblocking(False)
-            inputs.append(cli_out)
-            print('3389 connected...')
+def main():
+    # cli for public API
+    cli = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    cli.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    cli.bind(src_port)
 
-        elif s is cli_out:
-            print('3389 --> cli')
+    # main srv
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(src_port)
+    server.listen(5)
+
+    print('app running...')
+
+    while True:
+        cli_in, addr = server.accept()
+        cli_out = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        cli_out.connect(target_port)
+        th_in = TransThread(cli_in, cli_out)
+        th_out = TransThread(cli_out, cli_in)
+        th_in.start()
+        th_out.start()
+        th_in.join()
+        th_out.join()
+
+
+class TransThread(threading.Thread):
+    def __init__(self, cli_in, cli_out):
+        threading.Thread.__init__(self)
+        self.cli_in = cli_in
+        self.cli_out = cli_out
+
+    def run(self):
+        # blocking socket would slow the start speed down
+        while True:
+            data = ''
             try:
-                data = s.recv(BUFFER_SIZE)
-                cli_in.send(data)
+                data = self.cli_in.recv(BUFFER_SIZE)
             except:
-                print('A non-blocking 3389 --> cli')
-                pass
-        elif s is cli:
-            try:
-                data = s.recv(BUFFER_SIZE)
-                print('get IP:%s', data)
-            except:
-                print('A non-blocking get IP')
-                pass
-        elif s is cli_in:
-            # only cli_in is float
-            print('cli --> 3389')
-            try:
-                data = s.recv(BUFFER_SIZE)
-                while data != '':
-                    # should be here!!!!!!!!!!!
-                    print(len(data))
-                    cli_out.send(data)
-                    data = s.recv(BUFFER_SIZE)
+                self.cli_in.close()
+            if data != '':
+                try:
+                    self.cli_out.send(data)
+                except:
+                    self.cli_out.close()
+            else:
+                break
 
-            except:
-                print('error when cli_in recv')
-                pass
-        else:
-            print(s)
 
-cli.close()
-cli_in.close()
-cli_out.close()
-server.close()
+if __name__ == "__main__":
+    main()
